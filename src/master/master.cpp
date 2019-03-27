55ca34ecde6bc4375227b40122dff0a609fd96f9
Chun-Hung Hsiao <chhsiao@mesosphere.io>
2019-03-26 17:04:51 -0700

    Added authorization for applying `DESTROY_DISK` on `RAW` disks.
    Review: https://reviews.apache.org/r/70315

--- src/master/master.cpp
+++ src/master/master.cpp
@@ -3986,9 +3986,12 @@ Future<bool> Master::authorizeDestroyDisk(
       action = authorization::DESTROY_BLOCK_DISK;
       break;
     }
-    case Resource::DiskInfo::Source::UNKNOWN:
-    case Resource::DiskInfo::Source::PATH:
     case Resource::DiskInfo::Source::RAW: {
+      action = authorization::DESTROY_RAW_DISK;
+      break;
+    }
+    case Resource::DiskInfo::Source::UNKNOWN:
+    case Resource::DiskInfo::Source::PATH: {
       return Failure(
           "Failed to authorize principal '" +
           (principal.isSome() ? stringify(principal.get()) : "ANY") +
